997acec9fe5776b232f368eff992da78948ebce7
octo8ight <muse20first@gmail.com>
2024-06-04 17:14:00 +0800

    second

--- second_real_time.py
+++ second_real_time.py
@@ -1,56 +1,66 @@
+import requests
 import json
 import time
-import requests
-from datetime import datetime
 
-with open('wallets.json', 'r') as f:
-    data = json.load(f)
-    
-wallet_addresses = [d['address'] for d in data]
+url = "https://nd-326-444-187.p2pify.com/9de47db917d4f69168e3fed02217d15b/"
+payload = {
+    "id": 1,
+    "jsonrpc": "2.0",
+    "method": "getTokenLargestAccounts",
+    "params": ["2EBjqPYGLUExdWwJJRLqtGPawzb2aMjE1wTpUYKhy2UQ"]
+}
+headers = {
+    "accept": "application/json",
+    "content-type": "application/json"
+}
 
-token_contract = "2EBjqPYGLUExdWwJJRLqtGPawzb2aMjE1wTpUYKhy2UQ"
+def getLargetTokenHoldersWallet():
+    response = requests.post(url, json=payload, headers=headers)
+    data = response.json()
 
-def get_transaction_detail(tx_signature, wallet):
-    response = requests.post(
-        "https://api.mainnet-beta.solana.com", 
-        headers = {"Content-Type": "application/json"}, 
-        json={"jsonrpc":"2.0","id":1, "method": "getConfirmedTransaction", 
-              "params": [tx_signature, {"encoding": "jsonParsed","maxSupportedTransactionVersion":0}]}
-    )
-    
-    result = json.loads(response.text)
+    tokenHolders = []
+    lists = data['result']['value']
+    index = 0
+    for item in lists:
+        payload1 = {
+            "id": 1,
+            "jsonrpc": "2.0",
+            "method": "getAccountInfo",
+            "params": [
+                item['address'],
+                {
+                    "encoding": "jsonParsed",
+                    "commitment": "finalized"
+                }
+            ]
+        }
+        res = requests.post(url, headers=headers, json=payload1)
+        data1 = res.json()
+        tokenHolders.insert(index, {"address": data1['result']['value']['data']['parsed']['info']['owner'], 'amount': data1['result']['value']['data']['parsed']['info']['tokenAmount']['amount']})
+        index += 1
 
-    if 'result' in result:
-        if 'meta' in result['result'] and 'err' in result['result']['meta'] and result['result']['meta']['err'] == None:
-            if 'transaction' in result['result'] and 'message' in result['result']['transaction'] and 'accountKeys' in result['result']['transaction']['message']:
-                for item in result['result']['transaction']['message']['accountKeys']:
-                    if item['pubkey'] == token_contract:
-                        trans_time = datetime.fromtimestamp(result['result']['blockTime'])
-                        difference_time = datetime.now() - trans_time
-                        difference_time = difference_time.total_seconds() / 60
-                        if difference_time <= 7:
-                            print('wallet:  ', wallet, "       ", trans_time)
-                            break
-                        break
+    return tokenHolders
 
-def check_transactions(wallet):
-    response = requests.post(
-        "https://api.mainnet-beta.solana.com", 
-        headers = {"Content-Type": "application/json"}, 
-        json={"jsonrpc":"2.0", "id":1, "method":"getConfirmedSignaturesForAddress2",
-              "params":[wallet,{"limit":1}]}
-    )
-    
-    result = json.loads(response.text)
-    if 'result' in result:
-        for transaction in result['result']:
-            get_transaction_detail(transaction['signature'], wallet)
+def findWalletAndAmount(data, address):
+    for wallet in data:
+        if wallet['address'] == address:
+            return wallet['amount']
+    return -1
+
+def checkTokenAmountInWallet():
+    with open('wallets.json') as f:
+        data = json.load(f)
+
+    newData = getLargetTokenHoldersWallet()
+    for wallet in newData:
+        pastAmount = findWalletAndAmount(data=data, address=wallet['address'])
+        newAmount = wallet['amount']
+        if pastAmount != newAmount:
+            print(wallet['address'] + 'birb token balance changed.')
+
+    with open('wallets.json', 'w') as f:
+        json.dump(newData, f)
 
 while True:
-    for wallet in wallet_addresses:
-        check_transactions(wallet)
-    time.sleep(60*5)  # Check every 5 minutes
-    
-# for wallet in wallet_addresses:
-#     check_transactions(wallet)
-# get_transaction_detail('5LK5ig3L8oqfs2MYyNHtH2EJ7zvk4c2Ed9KMVmDpoLBRGdtr11y5wDYkHfv4dtxucfdjE9tddJzAx4in7V9A8Rm4', '5Q544fKrFoe6tsEbD7S8EmxGTJYAKtTVhAW5Q5pge4j1')
+    checkTokenAmountInWallet()
+    time.sleep(60*3)
